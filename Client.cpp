85a0a430b55304cc6ccea258bfd5113ab11a5f29
Raz <raz@yosigal.com>
2018-06-16 15:24:46 +0300

    Merge branch 'master' of https://github.com/RazK/os-ex4
    # Conflicts:
    #	CMakeLists.txt
    #	Client.h
    #	Server.cpp

--- Client.cpp
+++ Client.cpp
@@ -3,11 +3,24 @@
 //
 
 #include "Client.h"
+#include "Protocol.h"
 
 ErrorCode Client::_RequestCreateGroup(const std::string& groupName,
-                              const std::vector<const std::string&>& listOfClientNames) const
+                                      const std::string& listOfClientNames) const
 {
-    return ErrorCode::NOT_IMPLEMENTED;
+    // Init empty message
+    CreateGroupMessage msg; // type = CREATE_GROUP
+    bzero(&msg, sizeof(msg));
+
+    // Build message
+    msg.nameLen = groupName.length();
+    ASSERT(msg.nameLen <= WA_MAX_NAME);
+    msg.groupName = groupName.c_str();
+    msg.clientsLen = listOfClientNames.length();
+    msg.clientNames = listOfClientNames.c_str();
+
+    // Send message
+
 }
 ErrorCode Client::_RequestSendMessage(const std::string& targetName, const std::string& message) const
 {
--- main.cpp
+++ main.cpp
@@ -5,5 +5,11 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "sizeof enum = " << sizeof(command_type) << std::endl;
+    std::cout << "sizeof create_group_message = " << sizeof(CreateGroupMessage) << std::endl;
+    std::cout << "create_group_message.type = " << (char)(CreateGroupMessage().mtype + '0') <<
+                                                                                       std::endl;
+
+    std::cout << "who.type = " << (char)(WhoMessage().mtype + '0') <<
+              std::endl;
     return 0;
 }
