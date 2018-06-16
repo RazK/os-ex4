c983fb63da9b7d2f21513cedf8c97c5b85b865a4
heimy4prez <heimy4prez@gmail.com>
2018-06-16 19:27:14 +0300

    getting ready for server

--- ErrorCode.h
+++ ErrorCode.h
@@ -14,7 +14,7 @@ typedef enum _ErrorCode{
 
 #define ASSERT(cond, msg)                   \
 do {                                        \
-    if (!cond) {                             \
+    if (!(cond)) {                             \
         std::cerr <<  (msg) << std::endl;   \
         exit(-1);                           \
     }                                       \
@@ -38,8 +38,6 @@ do {                                        \
 #define ASSERTANDEXIT_SUCCESS(f, msg)       ASSERTANDEXIT((ErrorCode::SUCCESS != (f)), msg)
 #define ASSERTANDRET_SUCCESS(f, msg)        ASSERTANDRET((ErrorCode::SUCCESS != (f)), msg, ErrorCode::FAIL)
 #define ASSERTANDDO_SUCCESS(val, f)         ASSERTANDDO((ErrorCode::SUCCESS != (val)), f)
-
-
 #define ASSERTANDRET_NOT_NULL(obj, msg)     ASSERTANDRET((nullptr != (obj)), msg, ErrorCode::FAIL)
 
 #define ASSERT_WRITE(fd, buf, count)        ASSERT(count == write(fd, buf, count), "Write error")
